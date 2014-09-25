e6dd43f250e8c06c32f6e38ebaf3e934cfe076c9
osamabuhamad <osamabuhamad@aa0508-29.labs.sce.carleton.ca>
2014-09-25 13:30:37 -0400

    added classes

---
+++ src/CourseEvent.java
@@ -0,0 +1,16 @@
+import java.util.EventObject;
+
+
+public class CourseEvent extends EventObject {
+
+	public CourseEvent(Object arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
